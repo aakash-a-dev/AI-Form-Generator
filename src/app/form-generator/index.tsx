606a74d1dab1b741385c39c8298fa2161955b12f
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-24 00:57:26 +0530

    APi done

--- src/app/form-generator/index.tsx
+++ src/app/form-generator/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Dialog,
   DialogContent,
@@ -12,10 +12,35 @@ import {
 import { Button } from '@/components/ui/button'
 import { Form } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
+import { generateForm } from '../actions/generateForm';
+import { useFormState, useFormStatus } from 'react-dom';
 
+const initialState: {
+    message: string;
+    data?: any;
+} = {
+    message: "",
+}
+
+export function SubmitButton() {
+    const { pending } = useFormStatus();
+    return (
+        <Button type="submit" disabled={pending}>
+            {pending ? "Generating..." : "Generate"}
+        </Button>
+    )
+}
 
 export default function FormGenerator() {
+    const [state, formAction] = useFormState(generateForm, initialState)
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (state.message === "success") {
+            setOpen(false);
+        }
+        console.log(state.data)
+    }, [state.message])
 
     const onFormCreate = () => {
         setOpen(true);
@@ -26,16 +51,19 @@ export default function FormGenerator() {
   <DialogContent className='sm:max-w-[425px]'>
     <DialogHeader>
       <DialogTitle>Create New Form</DialogTitle>
-                  <form>
+              
+              </DialogHeader>
+                  <form action={formAction}>
                       <div className='gird gap-4 py-4'>
                           <Textarea id="description" name="description" required placeholder='GIve a Good Description 
                           for your form, to see the magic🪄'></Textarea>
                       </div>
-                 </form>
-              </DialogHeader>
               <DialogFooter>
+                  <SubmitButton/>
                   <Button variant="link">Create Manually</Button>
               </DialogFooter>
+                 </form>
+
   </DialogContent>
 </Dialog>
 
